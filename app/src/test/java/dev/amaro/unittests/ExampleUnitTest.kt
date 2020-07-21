67cc875127639e26264f7e0b8f8c19b9405d0e21
Rodrigo Amaro <rodrigo.amaro@payclip.com>
2020-07-21 09:32:03 -0500

    Initial

---
+++ app/src/test/java/dev/amaro/unittests/ExampleUnitTest.kt
@@ -0,0 +1,19 @@
+package dev.amaro.unittests
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import java.math.BigDecimal
+
+/**
+App Proposal: Currency converter
+Features:
+- Accept many currencies
+- Must check current conversion rate automatically
+- Must apply taxes from each country
+ **/
+
+
+class CurrencyConverterTest {
+
+}
