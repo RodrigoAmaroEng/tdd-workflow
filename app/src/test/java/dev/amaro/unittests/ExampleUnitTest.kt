3b2d95def37c7cb55564b7295970edec76bac6bf
Rodrigo Amaro <rodrigo.amaro@payclip.com>
2020-07-21 16:58:43 -0500

    Step 7

--- app/src/test/java/dev/amaro/unittests/ExampleUnitTest.kt
+++ app/src/test/java/dev/amaro/unittests/ExampleUnitTest.kt
@@ -12,20 +12,27 @@ Features:
 - Must apply taxes from each country
  **/
 
-// STEP 6: We can start informing the amount to convert
+// STEP 7: Now lets handle how the amount is presented after its conversion
+// The proposed solution happens to mess with tests so we have to check them
 class CurrencyConverterTest {
 
     @Test
     fun `Converting MXN to USD should return 22_59`() {
         val converter = Converter(provideRateAs(22.59))
-        assertEquals(22.59.toBigDecimal(), converter.convert("MXN", "USD"))
+        assertEquals(22.59.toBigDecimal(), converter.convert("MXN", "USD").value)
     }
 
     @Test
-    // Lets change this test to conform the proposal
     fun `Converting 2 USD to EUR should return 2_70`() {
         val converter = Converter(provideRateAs(1.35))
-        assertEquals(BigDecimal("2.70"), converter.convert("USD", "EUR", BigDecimal(2)))
+        assertEquals(BigDecimal("2.70"), converter.convert("USD", "EUR", Amount(BigDecimal(2))).value)
+    }
+
+    @Test
+    // The proposal is to present the value along its currency
+    fun `When print the amount it should present its currency`() {
+        val converter = Converter(provideRateAs(1.35))
+        assertEquals("2.70 EUR", converter.convert("USD", "EUR", Amount(BigDecimal(2))).toString())
     }
 
     private fun provideRateAs(rate: Double) = object : RateProvider {
@@ -36,17 +43,25 @@ class CurrencyConverterTest {
 }
 
 
+
+// VERSION 5: We have to change quite a bit piece of code to achieve it
 typealias Currency = String
-typealias Amount = BigDecimal
+
+data class Amount(val value: BigDecimal, val currency: Currency? = null) {
+    override fun toString(): String {
+        return "$value $currency"
+    }
+}
+
 typealias Rate = BigDecimal
 
 interface RateProvider {
     fun rateFor(from: Currency, to: Currency): Rate
 }
-// VERSION 4: We change the solution to calculate the converted amount
+
 class Converter(private val rateProvider: RateProvider) {
-    fun convert(from: Currency, to: Currency, amount: Amount = BigDecimal.ONE): Amount {
-        return rateProvider.rateFor(from, to).times(amount)
+    fun convert(from: Currency, to: Currency, amount: Amount = Amount(BigDecimal.ONE)): Amount {
+        return Amount(rateProvider.rateFor(from, to).times(amount.value), to)
     }
 }
 
