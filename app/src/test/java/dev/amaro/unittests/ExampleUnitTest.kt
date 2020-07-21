b2c6efe48fec62b4b4962f8d55ccdb0a056a1cfe
Rodrigo Amaro <rodrigo.amaro@payclip.com>
2020-07-21 09:34:57 -0500

    Step 1

--- app/src/test/java/dev/amaro/unittests/ExampleUnitTest.kt
+++ app/src/test/java/dev/amaro/unittests/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package dev.amaro.unittests
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 import java.math.BigDecimal
 
 /**
@@ -13,7 +12,22 @@ Features:
 - Must apply taxes from each country
  **/
 
-
+// STEP 1: Prioritize most valuable deliverable
+// In this case it can be converting (any) two currencies
 class CurrencyConverterTest {
-
+    @Test
+    fun `Converting MXN to USD should return 22_59`() {
+        val converter = Converter()
+        assertEquals(22.59.toBigDecimal(), converter.convert("MXN", "USD"))
+    }
 }
+
+// VERSION 1: Simple as possible to cover the proposed test
+class Converter {
+    fun convert(from: String, to: String): BigDecimal {
+        return 22.59.toBigDecimal()
+    }
+}
+
+
+
